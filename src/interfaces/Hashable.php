0bebfe4f33ada6b6d343850aaa7f5b0d988b866f
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-05-23 13:29:55 +0200

    Specify hash length

--- src/interfaces/Hashable.php
+++ src/interfaces/Hashable.php
@@ -11,7 +11,9 @@
 interface Hashable {
 
 	/**
-	 * Returns a hash based on the value of the object.
+	 * Returns a string hash based on the value of the object. The string must not exceed
+	 * 255 bytes (255 ASCII characters or less when it contains Unicode characters that
+	 * need to be UTF-8 encoded) to allow using indexes on all database systems.
 	 *
 	 * @since 0.1
 	 *
