f37baa00ac1606817feed425dcc256f27edb7f84
Itamar Givon <itamar.givon@wikimedia.de>
2020-10-21 11:37:48 +0200

    Remove Immutable

--- src/DataValues/DataValue.php
+++ src/DataValues/DataValue.php
@@ -3,13 +3,12 @@
 namespace DataValues;
 
 use Comparable;
-use Immutable;
 use Serializable;
 
 /**
  * Interface for objects that represent a single data value.
  */
-interface DataValue extends Comparable, Serializable, Immutable {
+interface DataValue extends Comparable, Serializable {
 
 	/**
 	 * Returns the identifier of the datavalues type.
--- tests/phpunit/DataValueTest.php
+++ tests/phpunit/DataValueTest.php
@@ -5,7 +5,6 @@ namespace DataValues\Tests;
 use Comparable;
 use DataValues\DataValue;
 use Exception;
-use Immutable;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
 use Serializable;
@@ -82,7 +81,6 @@ abstract class DataValueTest extends TestCase {
 	 * @dataProvider instanceProvider
 	 */
 	public function testImplements( DataValue $value, array $arguments ) {
-		$this->assertInstanceOf( Immutable::class, $value );
 		$this->assertInstanceOf( Comparable::class, $value );
 		$this->assertInstanceOf( Serializable::class, $value );
 		$this->assertInstanceOf( DataValue::class, $value );
